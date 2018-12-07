ad2d80625df94dcef5872324f5b6abb93a470cdf
Vuk Stankovic <vuks89@gmail.com>
2018-12-07 23:36:12 +0100

    Fix Java imports

--- src/android/Aronia.java
+++ src/android/Aronia.java
@@ -1,6 +1,12 @@
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+
+
+import android.content.Context;
+import android.content.Intent;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,6 +16,10 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class Aronia extends CordovaPlugin {
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
