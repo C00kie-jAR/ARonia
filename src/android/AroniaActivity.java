95fc16d8c31cbc00abdcf6d552c469a4f384a08b
Vuk Stankovic <vuks89@gmail.com>
2018-12-08 09:07:55 +0100

    Tozas part of code added

--- src/android/AroniaActivity.java
+++ src/android/AroniaActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import com.google.ar.sceneform.ux.ArFragment;
 import android.support.v7.app.AppCompatActivity;
 
+import android.service.autofill.TextValueSanitizer;
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.rendering.*;
+
 public class AroniaActivity extends AppCompatActivity {
     
     private ArFragment fragment;
@@ -16,6 +26,17 @@ public class AroniaActivity extends AppCompatActivity {
         setContentView(getApplication().getResources().getIdentifier("activity_aronia", "layout", package_name));
         fragment = (ArFragment)
                 getSupportFragmentManager().findFragmentById(getApplication().getResources().getIdentifier("activity_aronia", "id", package_name));
+
+        ArSceneView arView = fragment.getArSceneView();
+        Scene scene = arView.getScene();
+        Node andy = new Node();
+        andy.setParent(scene);
+        ModelRenderable.builder()
+                .setSource(this, getApplication().getResources().getIdentifier("raw/andy.sfb", null, package_name))
+                .build()
+                .thenAccept((renderable) -> {
+                    andy.setRenderable(renderable);
+                });
 
     }
 }
