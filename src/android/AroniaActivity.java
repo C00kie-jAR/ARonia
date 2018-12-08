ac609a19357e8f26d11085620150b6cf3b08a850
Vuk Stankovic <vuks89@gmail.com>
2018-12-08 12:06:20 +0100

    Final version for hackathon

--- src/android/AroniaActivity.java
+++ src/android/AroniaActivity.java
@@ -1,23 +1,19 @@
 package org.aronia.plugin;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.ux.ArFragment;
 import android.support.v7.app.AppCompatActivity;
-
-import android.service.autofill.TextValueSanitizer;
-import com.google.ar.core.Anchor;
-import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.ArSceneView;
-import com.google.ar.sceneform.Node;
-import com.google.ar.sceneform.Scene;
-import com.google.ar.sceneform.math.Vector3;
-import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.rendering.*;
 
 public class AroniaActivity extends AppCompatActivity {
     
     private ArFragment fragment;
+    private ModelRenderable duckRenderable;
+    private static final String GLTF_ASSET =
+            "https://raw.githubusercontent.com/AnalyticalGraphicsInc/cesium/master/Apps/SampleData/models/CesiumMilkTruck/CesiumMilkTruck-kmc.gltf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +23,32 @@ public class AroniaActivity extends AppCompatActivity {
         fragment = (ArFragment)
                 getSupportFragmentManager().findFragmentById(getApplication().getResources().getIdentifier("activity_aronia", "id", package_name));
 
-        ArSceneView arView = fragment.getArSceneView();
-        Scene scene = arView.getScene();
-        Node andy = new Node();
-        andy.setParent(scene);
         ModelRenderable.builder()
-                .setSource(this, getApplication().getResources().getIdentifier("raw/andy.sfb", null, package_name))
+                .setSource(this, RenderableSource.builder().setSource(
+                        this,
+                        Uri.parse(GLTF_ASSET),
+                        RenderableSource.SourceType.GLTF2)
+                        .setScale(0.5f)  // Scale the original model to 50%.
+                        .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                        .build())
+                .setRegistryId(GLTF_ASSET)
                 .build()
-                .thenAccept((renderable) -> {
-                    andy.setRenderable(renderable);
-                });
+                .thenAccept(renderable -> duckRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            System.out.println(throwable.getStackTrace().toString());
+                            return null;
+                        });
+        // ArSceneView arView = fragment.getArSceneView();
+        // Scene scene = arView.getScene();
+        // Node andy = new Node();
+        // andy.setParent(scene);
+        // ModelRenderable.builder()
+        //         .setSource(this, getApplication().getResources().getIdentifier("raw/andy.sfb", null, package_name))
+        //         .build()
+        //         .thenAccept((renderable) -> {
+        //             andy.setRenderable(renderable);
+        //         });
 
     }
 }
